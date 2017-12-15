fb2c1781c78a560da3674599e47b73a6206683d2
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2017-12-15 15:12:41 +0100

    Better require declaration

--- bots2/bot.js
+++ bots2/bot.js
@@ -1,5 +1,5 @@
-var moment = require('moment');
-const TelegramBot = require('node-telegram-bot-api');
+const moment = require('moment'),
+      TelegramBot = require('node-telegram-bot-api');
 
 const token = '123456789:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 
